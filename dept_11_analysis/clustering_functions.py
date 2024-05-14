170c1fd83380503e65b92e95ab4036eafb139ec3
CorgiCapyCode <corgicapycode@gmail.com>
2024-05-14 19:11:28 +0200

    Minor adjustment

--- dept_11_analysis/clustering_functions.py
+++ dept_11_analysis/clustering_functions.py
@@ -1,11 +1,16 @@
 import pandas as pd
 from sklearn.cluster import KMeans
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import numpy as np
 
 def clustering(df: pd.DataFrame):
     ohe_df = one_hot_encoding(df=df)
-    ohe_df.info()
+    # ohe_df.info()
+    
 
-    # k_clusters_all = kmeans_cluster_all(df=ohe_df)
+    k_clusters_all = kmeans_cluster_all(df=ohe_df)
+    visualization_of_clusters(df=ohe_df, clusters=k_clusters_all)
     
     return ohe_df
 
@@ -14,7 +19,7 @@ def one_hot_encoding(df: pd.DataFrame) -> pd.DataFrame:
     ohe_df = df.copy()
     # Converting the multi-level column index to one level & ensure all are of type str
     ohe_df.columns = ["_".join(map(str, col)) for col in df.columns]
-    ohe_df.columns = ohe_df.columns.astype(str)
+
     # Drop the first column containing the IDs   
     ohe_df.drop(columns=ohe_df.columns[0], inplace=True)
     
@@ -29,7 +34,22 @@ def kmeans_cluster_all(df: pd.DataFrame):
     kmeans.fit(df)
     clusters = kmeans.predict(df)
     return clusters
-    
+
+
+def visualization_of_clusters(df: pd.DataFrame, clusters: np.ndarray):
+    # Perform t-SNE for dimensionality reduction
+    tsne = TSNE(n_components=2, random_state=17)
+    tsne_features = tsne.fit_transform(df)
+
+    # Create scatter plot of the clusters
+    plt.figure(figsize=(10, 6))
+    plt.scatter(tsne_features[:, 0], tsne_features[:, 1], c=clusters, cmap='viridis', alpha=0.5)
+    plt.title('t-SNE Visualization of Clusters')
+    plt.xlabel('t-SNE Dimension 1')
+    plt.ylabel('t-SNE Dimension 2')
+    plt.colorbar(label='Cluster')
+    plt.show()
+
     
 
 def kmeans_cluster_group(df: pd.DataFrame, group: list):
